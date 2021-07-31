1a6c70f5d79bf3a01d4b7ae430a55fcce5996f7e
Andrey Kruglov <akruglov@metainfo.ru>
2021-07-31 11:37:46 +0300

    current

---
+++ codechef/FLOW001/Attempt01.cs
@@ -0,0 +1,25 @@
+﻿using System;
+ 
+public class Test
+{
+    public static void Main()
+    {
+        var firstLine   = Console.ReadLine();
+        var testsCount = int.Parse(firstLine);
+ 
+        for (int i = 1; i <= testsCount; i++)
+        {
+            var line  = Console.ReadLine().Split(' ');
+            var a = int.Parse(line[0]);
+            var b = int.Parse(line[1]);
+
+            var currentResult = Sum(a, b);
+            Console.WriteLine(currentResult);
+        }
+    }
+ 
+    private static int Sum(int a, int b)
+    {
+        return a + b;
+    }
+}
