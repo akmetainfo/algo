d626910acedcaf699ce43ed7b8eac3ee0e120f6d
Andrey Kruglov <akruglov@metainfo.ru>
2021-08-08 15:13:41 +0300

    current

---
+++ codechef/_Competitions/AUG21C/CHFINVNT/Etalon3.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+/*
+ChefAndBulbInvention(10,5,5).Dump(); // 2
+ChefAndBulbInvention(10,6,5).Dump(); // 4
+ChefAndBulbInvention(10,4,5).Dump(); // 9
+ChefAndBulbInvention(10,8,5).Dump(); // 8
+*/
+
+// https://www.codechef.com/AUG21C/problems/CHFINVNT
+public class Test
+{
+    public static void Main()
+    {
+        var firstLine = Console.ReadLine();
+        var testsCount = int.Parse(firstLine);
+
+        for (int i = 1; i <= testsCount; i++)
+        {
+            var line = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            var currentResult = ChefAndBulbInvention(line[0], line[1], line[2]);
+            Console.WriteLine(currentResult.ToString());
+        }
+    }
+
+    private static int ChefAndBulbInvention(int N, int p, int K)
+    {
+        var result = 0;
+
+        for (var i = 0; i < p % K; i++)
+        {
+            //result += (N - i) / K;
+            //result += (N - i) % K == 0 ? 1 : 0;
+            for (var j = 0; j < N - i; j = j + K)
+            {
+                //$"i={i} j={j}".Dump();
+                result++;
+            }
+        }
+        //$"------".Dump();
+        for (var j = p % K; j < N; j = j + K)
+        {
+            //$"i={i} j={j}".Dump();
+
+            result++;
+
+            if (j == p)
+                return result;
+        }
+        throw new Exception("unexpected");
+    }
+}
