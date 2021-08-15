50a7d9661f6556165b9278856dab135a3b7525ee
Andrey Kruglov <akruglov@metainfo.ru>
2021-08-15 20:40:21 +0300

    current

---
+++ codechef/_Competitions/START8C/JAVELIN/Attempt01.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+/*
+    JavelinQualification(3, 8000, 2, new int[] { 5000,5001,5002 }).Dump(); // 2 2 3
+*/
+
+// https://www.codechef.com/START8C/problems/JAVELIN
+public class Test
+{
+    public static void Main()
+    {
+        var firstLine = Console.ReadLine();
+        var testsCount = int.Parse(firstLine);
+
+        for (int i = 1; i <= testsCount; i++)
+        {
+            var line1 = Console.ReadLine().Split(' ');
+            var N = int.Parse(line1[0]);
+            var M = int.Parse(line1[0]);
+            var X = int.Parse(line1[2]);
+
+            var line2 = Console.ReadLine().Split(' ');
+            var nums = new int[N];
+            for (var a = 0; a < N; a++)
+            {
+                nums[a] = int.Parse(line2[a]);
+            }
+
+            var currentResult = JavelinQualification(N, M, X, nums);
+            Console.WriteLine(currentResult);
+        }
+    }
+
+    public static string JavelinQualification(int N, int M, int X, int[] nums)
+    {
+        var data = nums.Select((x, i) => new { id = i + 1, val = x });
+
+        var results = new List<int>();
+
+        results.AddRange(data.Where(x => x.val >= M).Select(x => x.id));
+
+        var missing = N - results.Count();
+        results.AddRange(data.Where(x => x.val < M).OrderByDescending(x => x.id).Select(x => x.id).Take(missing));
+
+        var ids = results.ToArray();
+        Array.Sort(ids);
+
+        var sb = new StringBuilder();
+
+        sb.Append(ids.Length);
+        sb.Append(' ');
+
+        sb.Append(string.Join(" ", ids));
+
+        return sb.ToString();
+    }
+}
---
+++ codechef/_Competitions/START8C/PENALTY/Attempt01.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+/*
+    PenaltyShots(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }).Dump(); // 0
+    PenaltyShots(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 }).Dump(); // 2
+    PenaltyShots(new int[] { 1, 0, 1, 0, 0, 0, 0, 0, 0, 0 }).Dump(); // 1
+    PenaltyShots(new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 }).Dump(); // 1
+*/
+
+// https://codeforces.com/contest/1559/problem/A
+public class Test
+{
+    public static void Main()
+    {
+        var firstLine = Console.ReadLine();
+        var testsCount = int.Parse(firstLine);
+
+        for (int i = 1; i <= testsCount; i++)
+        {
+            var nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            var currentResult = PenaltyShots(nums);
+            Console.WriteLine(currentResult);
+        }
+    }
+
+    public static int PenaltyShots(int[] nums)
+    {
+        var sum1 = 0;
+        var sum2 = 0;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (i % 2 == 0)
+                sum1 += nums[i];
+            else
+                sum2 += nums[i];
+        }
+
+        if (sum1 > sum2)
+            return 1;
+
+        if (sum1 < sum2)
+            return 2;
+
+        return 0;
+    }
+}
